2afe586b2a884fb4cd2adc5e1886fd2359cc7698
chakrichithimalli <143122577+chakrichithimalli@users.noreply.github.com>
2024-02-11 13:39:31 -0600

    Update SecondActivity.kt

--- CSCI-783-assignment-2/app/src/main/java/com/example/assignment_2_App/SecondActivity.kt
+++ CSCI-783-assignment-2/app/src/main/java/com/example/assignment_2_App/SecondActivity.kt
@@ -1,16 +1,22 @@
 package com.example.assignment_2_App
-
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 
-class SecondActivity : AppCompatActivity() {
+class SecondaryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_two)
 
         val textViewChallenges = findViewById<TextView>(R.id.textViewChallenges)
-        textViewChallenges.text = getString(R.string.challenges_list)
+        val challengesList = resources.getStringArray(R.array.challenges_list_array)
+        val formattedChallenges = StringBuilder()
+
+        for ((index, challenge) in challengesList.withIndex()) {
+            formattedChallenges.append("${index + 1}. $challenge\n")
+        }
+
+        textViewChallenges.text = formattedChallenges.toString()
     }
 }
