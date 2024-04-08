a5fe7f4de659f43edd7598d8ba07cd6f55372fb0
chakrichithimalli <143122577+chakrichithimalli@users.noreply.github.com>
2024-04-08 10:40:29 -0500

    Update MainActivity.kt

--- CSCI-783-assignment-2/app/src/main/java/com/example/assignment_2_App/MainActivity.kt
+++ CSCI-783-assignment-2/app/src/main/java/com/example/assignment_2_App/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.example.assignment_2_App
-
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.example.myapplication.R.layout.activity_main)
 
         val startExplicitlyButton: Button = findViewById(R.id.startExplicitlyButton)
         val startImplicitlyButton: Button = findViewById(R.id.startImplicitlyButton)
+        val viewImageQualityButton : Button = findViewById(R.id.viewImageQuality)
+
 
         startExplicitlyButton.setOnClickListener {
-            val explicitIntent = Intent(this, SecondActivity::class.java)
+            val explicitIntent = Intent(this, SecondaryActivity::class.java)
             startActivity(explicitIntent)
         }
 
@@ -23,5 +26,11 @@ class MainActivity : AppCompatActivity() {
             val implicitIntent = Intent("com.example.assignmentapp.START_SECOND_ACTIVITY")
             startActivity(implicitIntent)
         }
+
+        viewImageQualityButton.setOnClickListener {
+            val implicitIntent = Intent(this, ViewImageQuality::class.java)
+            startActivity(implicitIntent)
+        }
+
     }
 }
