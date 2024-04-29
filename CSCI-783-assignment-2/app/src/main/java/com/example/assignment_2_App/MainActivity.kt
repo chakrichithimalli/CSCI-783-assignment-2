ea28786d0362ba7cc3f76bd061d85860c7a4e656
chakrichithimalli <143122577+chakrichithimalli@users.noreply.github.com>
2024-04-29 15:32:24 -0500

    Update MainActivity.kt

--- CSCI-783-assignment-2/app/src/main/java/com/example/assignment_2_App/MainActivity.kt
+++ CSCI-783-assignment-2/app/src/main/java/com/example/assignment_2_App/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.assignment_2_App
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import android.Manifest
+import androidx.core.app.ActivityCompat
 import com.example.myapplication.R
 
 
@@ -11,10 +14,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.myapplication.R.layout.activity_main)
-
+        val REQUEST_CODE = 100
         val startExplicitlyButton: Button = findViewById(R.id.startExplicitlyButton)
         val startImplicitlyButton: Button = findViewById(R.id.startImplicitlyButton)
-        val viewImageQualityButton : Button = findViewById(R.id.viewImageQuality)
+        val viewImageQualityButton: Button = findViewById(R.id.viewImageQuality)
 
 
         startExplicitlyButton.setOnClickListener {
@@ -26,11 +29,23 @@ class MainActivity : AppCompatActivity() {
             val implicitIntent = Intent("com.example.assignmentapp.START_SECOND_ACTIVITY")
             startActivity(implicitIntent)
         }
-
         viewImageQualityButton.setOnClickListener {
-            val implicitIntent = Intent(this, ViewImageQuality::class.java)
-            startActivity(implicitIntent)
+            // Check camera permission before starting the activity to capture image
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // Requesting the permission for camera using the Manifest
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    REQUEST_CODE
+                )
+            } else {
+                // if Permission already granted, ViewImageActivity to capture image
+                startActivity(Intent(this, ViewImageQuality::class.java))
+            }
         }
-
     }
 }
